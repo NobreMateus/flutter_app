a3f79afd71b0b13d583adefbb9bec7f63646b3dc
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-06-03 11:28:06 -0300

    Inittial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,6 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
